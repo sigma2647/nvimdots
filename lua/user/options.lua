4ce5bb1c9be0f77af528f7ab1728d6a65ee58682
sigma2647 <1169446204@qq.com>
2024-03-06 05:46:22 +0800

    pda

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,6 @@
 local options = {
 	-- Example
 	-- fillchars="eob: "               -- hide ~ if it's empty line
-	winbar = "%=%m %f",
 	whichwrap = "b,s,<,>,[,],h,l",
 	mouse = "a",
 	termguicolors = true, -- hi
