749dab027a57eff64d0420a1da0d6244fbd07ef3
sigma2647 <1169446204@qq.com>
2024-03-11 03:42:11 +0800

    get winbar

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,3 +1,24 @@
 return {
+
   "nvimdev/lspsaga.nvim",
+  event = "LspAttach",
+  config = function()
+    require("lspsaga").setup({
+      symbol_in_winbar = {
+        enable = true,
+        show_file = true,
+        color_mode = true,
+        delay = 100,
+
+        hide_keyword = true,
+        folder_level = 0,
+      },
+      lightbulb = {
+        sign = false,
+      },
+      outline = {
+        layout = "float",
+      },
+    })
+  end,
 }
